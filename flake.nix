03cfc27f562d29f15ec42dce133adb62b92c9e3e
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2025-03-09 19:29:27 +0800

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "基于 nix 的 hadoop docker ";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }: let
+    allSystems = flake-utils.lib.allSystems;
+  in (
+    flake-utils.lib.eachSystem allSystems
+    (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      # doCheck will fail at write files
+      packages = rec {
+        docker_builder = pkgs.dockerTools.buildLayeredImage {
+          name = "hadoop_docker";
+          tag = "latest";
+          contents = with pkgs; [
+            hadoop_3_3
+            cacert
+            bashInteractive
+          ];
+          config = {
+            Cmd = ["bash"];
+          };
+        };
+
+        default = docker_builder;
+      };
+      formatter = pkgs.alejandra;
+    })
+  );
+}
