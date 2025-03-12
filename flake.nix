8ce1a581ca74d535d01030ae57d62069944ed829
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2025-03-12 17:51:29 +0800

    failed

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   description = "基于 nix 的 hadoop docker ";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/0e74ca98a74bc7270d28838369593635a5db3260";
+  inputs.flake-utils.url = "github:numtide/flake-utils/11707dc2f618dd54ca8739b309ec4fc024de578b";
 
   outputs = {
     self,
@@ -15,18 +15,30 @@
     (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      # doCheck will fail at write files
       packages = rec {
         docker_builder = pkgs.dockerTools.buildLayeredImage {
           name = "hadoop_docker";
-          tag = "latest";
+          tag = "3.3";
+          created = "now";
           contents = with pkgs; [
             hadoop_3_3
+            # CA for secure connect
             cacert
+            # bash
             bashInteractive
+            which
+            # vim editor
+            vim
+            # coreutils for some common command
+            coreutils-full
+            openjdk
           ];
+
+          enableFakechroot = true;
           config = {
-            Cmd = ["${pkgs.bashInteractive}/bin/bash"];
+            Cmd = [
+              "${pkgs.bashInteractive}/bin/bash"
+            ];
           };
         };
 
